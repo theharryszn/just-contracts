cde84f742410dabd48643ce23bf125d5510888e3
Abidemi Ope <opeabidemi@gmail.com>
2022-07-20 04:41:15 +0100

    fix: deps error

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,88 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+import yargs from 'yargs/yargs';
+
+const argv = yargs()
+  .env('')
+  .options({
+    coverage: {
+      type: 'boolean',
+      default: false,
+    },
+    gas: {
+      alias: 'enableGasReport',
+      type: 'boolean',
+      default: false,
+    },
+    gasReport: {
+      alias: 'enableGasReportPath',
+      type: 'string',
+      implies: 'gas',
+      default: undefined,
+    },
+    mode: {
+      alias: 'compileMode',
+      type: 'string',
+      choices: ['production', 'development'],
+      default: 'development',
+    },
+    ir: {
+      alias: 'enableIR',
+      type: 'boolean',
+      default: false,
+    },
+    compiler: {
+      alias: 'compileVersion',
+      type: 'string',
+      default: '0.8.9',
+    },
+    coinmarketcap: {
+      alias: 'coinmarketcapApiKey',
+      type: 'string',
+    },
+  })
+  .parseSync();
+
+if (argv.gas) {
+  require('hardhat-gas-reporter');
+}
+
+const withOptimizations = argv.gas || argv.compileMode === 'production';
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.9',
+  solidity: {
+    version: argv.compiler,
+    settings: {
+      optimizer: {
+        enabled: withOptimizations,
+        runs: 200,
+      },
+      viaIR: withOptimizations && argv.ir,
+    },
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 10000000,
+      allowUnlimitedContractSize: !withOptimizations,
+    },
+  },
+  gasReporter: {
+    showMethodSig: true,
+    currency: 'USD',
+    outputFile: argv.gasReport,
+    coinmarketcap: argv.coinmarketcap,
+  },
 };
+
+if (argv.coverage) {
+  require('solidity-coverage');
+  if (config.networks?.hardhat) {
+    config.networks.hardhat.initialBaseFeePerGas = 0;
+  }
+}
 
 export default config;
